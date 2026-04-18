08728c649ab3994d6f3d5336ecb12b1d67ca3e49
masaki-y <rendering9498@outlook.jp>
2026-04-18 16:29:54 +0900

    ms-gamebarポップアップ抑止のコマンド追加

--- desktop/windows/InitWinSetup/InitWinSetup.bat
+++ desktop/windows/InitWinSetup/InitWinSetup.bat
@@ -30,6 +30,24 @@ timeout 1
 reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t "REG_DWORD" /d "0" /f
 timeout 1
 
+:: Remove ms-gamebar popup
+reg add HKCR\ms-gamebar /f /ve /d URL:ms-gamebar 2>&1 >''
+timeout 1
+reg add HKCR\ms-gamebar /f /v "URL Protocol" /d "" 2>&1 >''
+timeout 1
+reg add HKCR\ms-gamebar /f /v "NoOpenWith" /d "" 2>&1 >''
+timeout 1
+reg add HKCR\ms-gamebar\shell\open\command /f /ve /d "\\"$env:SystemRoot\System32\systray.exe\"" 2>&1 >''
+timeout 1
+reg add HKCR\ms-gamebarservices /f /ve /d URL:ms-gamebarservices 2>&1 >''
+timeout 1
+reg add HKCR\ms-gamebarservices /f /v "URL Protocol" /d "" 2>&1 >''
+timeout 1
+reg add HKCR\ms-gamebarservices /f /v "NoOpenWith" /d "" 2>&1 >''
+timeout 1
+reg add HKCR\ms-gamebarservices\shell\open\command /f /ve /d "\\"$env:SystemRoot\System32\systray.exe\"" 2>&1 >''
+timeout 1
+
 :: OneDrive
 echo Uninstalling OneDrive
 onedrivesetup /uninstall
